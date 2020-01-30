99a2627773baf31013acd9edaab0225dda77c81a
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-01-30 16:57:49 +0100

    Data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+  { id: 1, title: 'Wiadomość numer 1', body: 'Zawartość wiadomości numer 1' },
+  { id: 2, title: 'Wiadomość numer 2', body: 'Zawartość wiadomości numer 2' },
+]
 
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości numer ${index}`
+  })  
+}, 2000)
+
+class App extends React.Component {
+  state = { 
+    comments: [...data]
+  }
+
+  getData = () => {
+    if(this.state.comments.length !== data.length) {
+      this.setState({
+        comments: [...data]
+      })
+  }
+  }
+
+  componentDidMount() {
+    this.idInt = setInterval(this.getData, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idInt)
+  }
+
+  render() { 
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id} >
+        <h4>{comment.title}</h4>
+        <p>{comment.body}</p>
+      </div>
+    ))
+    return ( 
+      <div>
+        {comments}
+      </div>
+     );
+  }
+}
+ 
 export default App;
